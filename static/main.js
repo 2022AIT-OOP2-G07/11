5be2622811c91bfa94271fbf836b319842be5928
hapiharu012 <120043995+hapiharu012@users.noreply.github.com>
2022-12-16 01:21:49 +0900

    index.cssの変更とmain.jsの整形

--- static/main.js
+++ static/main.js
@@ -2,36 +2,36 @@
 const btn = document.querySelector('button');
 const input = document.querySelector('input');
 const afterUpload = () => {
-  btn.disabled = false;
-  btn.value = "アップロード";
-  input.value = "";
-  formData = new FormData();
+    btn.disabled = false;
+    btn.value = "アップロード";
+    input.value = "";
+    formData = new FormData();
 }
 
 btn.addEventListener('click', (e) => {
-  e.preventDefault();
-  const file = input.files[0];
-  if (file === undefined) {
-    alert("ファイルが選択されていません");
-    return;
-  }
-  const formData = new FormData();
-  btn.disabled = true;
-  btn.value = "アップロード中"
-  formData.append("img_file", file);
-  fetch('/upload',{
-    method: "POST",
-    body: formData
-  }).then(res => {
+    e.preventDefault();
+    const file = input.files[0];
+    if (file === undefined) {
+        alert("ファイルが選択されていません");
+        return;
+    }
+    const formData = new FormData();
+    btn.disabled = true;
+    btn.value = "アップロード中"
+    formData.append("img_file", file);
+    fetch('/upload',{
+        method: "POST",
+        body: formData
+    }).then(res => {
     res.json().then(data => {
-      if (data.status === 'false') {
-        alert(data.message);
-      } else if (data.status === 'true') {
-        alert("送信が完了しました");
-      } else {
-        alert("ファイルサイズが1MBを超えていませんか？");
-      }
-      afterUpload();
+        if (data.status === 'false') {
+            alert(data.message);
+        } else if (data.status === 'true') {
+            alert("送信が完了しました");
+        } else {
+            alert("ファイルサイズが1MBを超えています");
+        }
+        afterUpload();
     });
-  });
+    });
 });
