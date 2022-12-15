a76c174c2b0259b6b2f6860c2f1c2291e8109945
k21139 <k21139kk@aitech.ac.jp>
2022-12-15 16:12:03 +0900

    GrayScale追加しました

--- Grayscale.py
+++ Grayscale.py
@@ -1,5 +1,5 @@
+# ライブラリインポート
 import cv2
-
 # グレースケール化
 # 画像読み込み
 img = cv2.imread('images/upload/Sample-image.jpeg')
@@ -9,8 +9,8 @@ img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('images/result/grayscale-fillter/Sample-image.jpeg', img_gray)
 
 # # 二値化
-# img2 = cv2.imread('images/upload/Sample-image.jpeg',0)
+# img2 = cv2.imread('before_picture/Sample-image.jpeg',0)
 # threshold = 100
 # # 二値化(閾値100超えた画素を255)
 # ret, img_thresh = cv2.threshold(img2, threshold, 255, cv2.THRESH_BINARY)
-# cv2.imwrite('images/result/grayscale-fillter/Niti_Sample-image.jpeg', img_thresh)
+# cv2.imwrite('after_picture/nitiSample-image.jpeg', img_thresh)
