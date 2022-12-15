8870240c5c68df159c44259027cc250294cf26a4
k21139 <k21139kk@aitech.ac.jp>
2022-12-15 15:51:40 +0900

    Grayscale書きました

---
+++ Grayscale.py
@@ -0,0 +1,16 @@
+import cv2
+
+# グレースケール化
+# 画像読み込み
+img = cv2.imread('images/upload/Sample-image.jpeg')
+# 読み込んだ画像グレースケール化
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 画像保存
+cv2.imwrite('images/result/grayscale-fillter/Sample-image.jpeg', img_gray)
+
+# # 二値化
+# img2 = cv2.imread('images/upload/Sample-image.jpeg',0)
+# threshold = 100
+# # 二値化(閾値100超えた画素を255)
+# ret, img_thresh = cv2.threshold(img2, threshold, 255, cv2.THRESH_BINARY)
+# cv2.imwrite('images/result/grayscale-fillter/Niti_Sample-image.jpeg', img_thresh)
